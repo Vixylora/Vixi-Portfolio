9adba17802d5f3fbebe29820939ffaba57ddb267
Vixi <benamourtvoff@gmail.com>
2025-10-03 19:23:56 +0100

    To the previous commit, Landing page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
+import Landing from './Components/Content/Landing'
 import Navigation from './Navigation'
-import TargetCursor from "./Components/CursorTarget/TargetCursor";
 
 function App() {
   return (
     <>
-      {/* <div id="Custom-Cursor">
-        <TargetCursor spinDuration={5} hideDefaultCursor={false} />
-      </div> */}
       <Navigation />
+      <Landing />
     </>
   )
 }
