bcf33e093c7dc7e5223ec82ed49f1c85da25185d
Vixi <benamourtvoff@gmail.com>
2025-10-08 13:50:49 +0100

    Hero section added

--- src/Components/Content/HeroSection/Hero.jsx
+++ src/Components/Content/HeroSection/Hero.jsx
@@ -1,20 +1,43 @@
-import "./Hero.css"
-import { Herodata, Heropara } from "./HeroData"
+import "./Hero.css";
+import "./Infinitytext.css";
+import { Herodata, Heropara } from "./HeroData";
 
-import profilepic from "../../../assets/profilepic.jpeg"
+import { RiScrollToBottomFill } from "react-icons/ri";
+import { BiSolidDonateHeart } from "react-icons/bi";
+import profilepic from "../../../assets/profilepic.jpeg";
 
 function Hero() {
-    return(
-        <>
-            <img src={profilepic} id="Hero-img" />
-            <div id="Hero-text">
-                <div id="heroheadcontainer">
-                    <h2 id="Hero-head">Hello, I am <span id="Hero-important">Vixy</span></h2>
-                </div>
-                <p id="Hero-p"></p>
+  const Donation = "";
+  return (
+    <div id="Hero-container">
+      <div id="Hero-wrapper">
+        <img src={profilepic} id="Hero-img" className="cursor-target" />
+        <div id="wrapper">
+          {Herodata.map((items, index) => {
+            return <div className={items.cName}>{items.text}</div>;
+          })}
+        </div>
+        <div id="Hero-text">
+          <h2 id="Hero-head">
+            <div>
+              Hello, I am <span id="Hero-important">Vixi</span>
             </div>
-        </>
-    )
+            <div id="hero-divider" />
+          </h2>
+          <p id="Hero-p" className="cursor-target">{Heropara}</p>
+        </div>
+        <div id="Hero-buttons">
+          <a href="#aboutme">
+            <button className="hero-btn btn1 cursor-target">
+              Learn More
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
+
+// BACK
--- src/Components/Content/HeroSection/HeroData.jsx
+++ src/Components/Content/HeroSection/HeroData.jsx
@@ -1,32 +1,36 @@
-export const Heropara = "~~~"
+export const Heropara = "Building interactive, responsive, and visually appealing web applications that provide seamless user experiences."
 
 export const Herodata = [
     {
         text: "Web developer",
-        cName: "item item1",
+        cName: "item item1 cursor-target",
     },
     {
         text: "UI designer",
-        cName: "item item2",
+        cName: "item item2 cursor-target",
     },
     {
         text: "Logic",
-        cName: "item item3",
+        cName: "item item3 cursor-target",
     },
     {
         text: "Web designer",
-        cName: "item item4",
+        cName: "item item4 cursor-target",
     },
     {
         text: "App designer",
-        cName: "item item5",
+        cName: "item item5 cursor-target",
     },
     {
         text: "Someone",
-        cName: "item item6",
+        cName: "item item6 cursor-target",
     },
     {
-        text: "Special",
-        cName: "item item7",
+        text: "Front-end dev",
+        cName: "item item7 cursor-target",
+    },
+    {
+        text: "Roblox developer",
+        cName: "item item8 cursor-target",
     }
 ]
