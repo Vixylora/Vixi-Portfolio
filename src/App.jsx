d1c98ab1a01e12a5fe63bfafb22d2cb7976f21bd
Vixi <benamourtvoff@gmail.com>
2025-10-08 13:47:22 +0100

    Target Cursor Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Landing from './Components/Content/Landing'
 import Navigation from './Navigation'
+import TargetCursor from './Components/CursorTarget/TargetCursor'
 
 function App() {
   return (
     <>
+      <TargetCursor spinDuration={4} hideDefaultCursor={true} />
       <Navigation />
       <Landing />
     </>
