d48589908b80a1709fe1a85c8a0271dea7866055
Vixi <benamourtvoff@gmail.com>
2025-10-19 12:11:23 +0100

    Center all elements within the landing page

--- src/Components/Content/Landing.jsx
+++ src/Components/Content/Landing.jsx
@@ -1,10 +1,13 @@
 import Hero from "./HeroSection/Hero";
+import AboutMe from "./AboutMe/aboutme";
+import "./landing.css";
 
 function Landing() {
     return(
-        <>
+        <div id="landing">
             <Hero />
-        </>
+            <AboutMe />
+        </div>
     )
 }
 
