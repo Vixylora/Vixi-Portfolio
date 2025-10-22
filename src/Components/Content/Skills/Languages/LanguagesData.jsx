7a31cf107fbb87a00ed604c445785798cdc7abb4
Vixi <benamourtvoff@gmail.com>
2025-10-22 18:57:31 +0100

    Added languages data

---
+++ src/Components/Content/Skills/Languages/LanguagesData.jsx
@@ -0,0 +1,69 @@
+import { Colorselector, widthselector } from "../Profeciency.jsx";
+
+const LanguagesData = [
+    {
+        name: "Typescript",
+        level: "Forging",
+        color: Colorselector("Forging"),
+        width: widthselector("Forging"),
+        classN: "Lang-select cursor-target",
+        className: "Lang-title",
+        classLevel: "Lang-level"
+    },
+    {
+        name: "JavaScript",
+        level: "Tempered",
+        color: Colorselector("Tempered"),
+        width: widthselector("Tempered"),
+        classN: "Lang-select cursor-target",
+        className: "Lang-title",
+        classLevel: "Lang-level"
+    },
+    {
+        name: "CSS3",
+        level: "Honed",
+        color: Colorselector("Honed"),
+        width: widthselector("Honed"),
+        classN: "Lang-select cursor-target",
+        className: "Lang-title",
+        classLevel: "Lang-level"
+    },
+    {
+        name: "HTML5",
+        level: "Mastered",
+        color: Colorselector("Mastered"),
+        width: widthselector("Mastered"),
+        classN: "Lang-select cursor-target",
+        className: "Lang-title",
+        classLevel: "Lang-level"
+    },
+    {
+        name: "",
+        level: "",
+        color: "",
+        width: "",
+        classN: "seperator",
+        className: "seperator-title",
+        classLevel: "seperator-level"
+    },
+    {
+        name: "React",
+        level: "Tempered",
+        color: Colorselector("Tempered"),
+        width: widthselector("Tempered"),
+        classN: "Lang-select cursor-target",
+        className: "Lang-title",
+        classLevel: "Lang-level"
+    },
+    {
+        name: "Tailwind",
+        level: "Honed",
+        color: Colorselector("Honed"),
+        width: widthselector("Honed"),
+        classN: "Lang-select cursor-target",
+        className: "Lang-title",
+        classLevel: "Lang-level"
+    }
+]
+
+export default LanguagesData;
