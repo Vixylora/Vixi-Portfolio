97773f9e5ad3f1b487c1bc460b7fad1fc73897b5
Vixi <benamourtvoff@gmail.com>
2025-10-22 18:58:18 +0100

    added skills section and css styling

--- src/Components/Content/Skills/Skills.jsx
+++ src/Components/Content/Skills/Skills.jsx
@@ -1,9 +1,15 @@
-import "Skill.css";
+import "./Skills.css";
+import SkillApp from "./Apps/SkillApp";
+import SkillLang from "./Languages/SkillLang";
 
 function Skill() {
     return(
-        <div>
-            f
+        <div id="Skills-holder">
+            <h2 id="skill-title">Skills <div id="divider" /></h2>
+            <div id="Skills">
+                <SkillApp />
+                <SkillLang />
+            </div>
         </div>
     )
 }
