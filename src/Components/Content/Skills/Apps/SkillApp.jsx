83217f6e3fb5b19e79fc2545e18c940471f3cf4a
Vixi <benamourtvoff@gmail.com>
2025-10-29 12:46:25 +0100

    Finished skills sections

--- src/Components/Content/Skills/Apps/SkillApp.jsx
+++ src/Components/Content/Skills/Apps/SkillApp.jsx
@@ -1,8 +1,21 @@
 import "./SkillApp.css";
+import AppList from "./AppData.jsx";
 
 function SkillApp() {
     return(
-        <></>
+        <div id="Skillapps">
+            {AppList.map((items, index) => {
+                return(
+                    <div key={index} className={items.appclass}>
+                        <img src={items.img} className={items.imgclass} />
+                        <div className="text-container">
+                            <h3 className={items.titleclass}>{items.name}</h3>
+                            <p className={items.profeciencyclass}>{items.profeciency}</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
