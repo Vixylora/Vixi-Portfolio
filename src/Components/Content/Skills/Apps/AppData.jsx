14063a7023333490b402a6a923c74092c9e16afd
Vixi <benamourtvoff@gmail.com>
2025-10-29 12:46:00 +0100

    Apps Data added

--- src/Components/Content/Skills/Apps/AppData.jsx
+++ src/Components/Content/Skills/Apps/AppData.jsx
@@ -1,13 +1,56 @@
 // Forging / Tempered / Honed / Mastered / Transcendant
-
-const Apps = {
-    img: "",
-    name: "",
-    profeciency: "",
-    className: "",
-}
+import Figma from "./AppIcons/figma.png";
+import RobloxStudio from "./AppIcons/RobloxStudio.png";
+import VScode from "./AppIcons/VScode.png";
+import Git from "./AppIcons/Git.png";
+import GitHub from "./AppIcons/GitHub.png";
 
 const AppList = [
-    {img:"", name: "Figma", className: ""},
+  {
+    appclass: "appclass cursor-target",
+    img: RobloxStudio,
+    imgclass: "image-size",
+        name: "Roblox Studio",
+    titleclass: "title-class",
+    profeciency: "Honed",
+    profeciencyclass: "profeciency-class",
+  },
+  {
+    appclass: "appclass cursor-target",
+    img: Figma,
+    imgclass: "image-size",
+        name: "Figma",
+    titleclass: "title-class",
+    profeciency: "Mastered",
+    profeciencyclass: "profeciency-class",
+  },
+  {
+    appclass: "appclass cursor-target",
+    img: VScode,
+    imgclass: "image-size",
+        name: "Visual Studio Code",
+    titleclass: "title-class",
+    profeciency: "Mastered",
+    profeciencyclass: "profeciency-class",
+  },
+  {
+    appclass: "appclass cursor-target",
+    img: Git,
+    imgclass: "image-size",
+        name: "Git",
+    titleclass: "title-class",
+    profeciency: "Honed",
+    profeciencyclass: "profeciency-class",
+  },
+  {
+    appclass: "appclass cursor-target",
+    img: GitHub,
+    imgclass: "image-size",
+        name: "GitHub",
+    titleclass: "title-class",
+    profeciency: "Honed",
+    profeciencyclass: "profeciency-class",
+  },
+];
 
-]
+export default AppList;
